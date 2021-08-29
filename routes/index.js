07d61b6f622afa51001944a90dbd2ae0efd397ac
井上翼 <tsubasa.inoue.3b@stu.hosei.ac.jp>
2021-08-29 16:13:43 +0900

    first

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require("express");
 var router = express.Router();
+var errorRouter = require("../controllers/errorController");
 
 var indexController = require("../controllers/index");
 
 router.get("/",indexController.index);
+
+router.get("/a",(req,res,next) => {
+    var err = new Error();
+    console.log("ssss");
+    res.status(500);
+    res.locals.status = 500; //仮　エラー時にlocal変数として、ステータスコードを書き込んでおく
+    next(err);
+});
+
+//router.use(errorRouter.internalServerError);
 
 module.exports = router;
