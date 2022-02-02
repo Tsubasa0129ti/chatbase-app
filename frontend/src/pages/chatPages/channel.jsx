d3b492b49f775f7bcde1b980cc40fc41f0065101
井上翼 <tsubasa.inoue.3b@stu.hosei.ac.jp>
2022-02-02 18:17:41 +0900

    revise only fundamental function in chat. ex. message from database and from socket

--- frontend/src/pages/chatPages/channel.jsx
+++ frontend/src/pages/chatPages/channel.jsx
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import socketIOClient from 'socket.io-client';
 import uuid from 'react-uuid';
+import { useLocation, useHistory } from 'react-router-dom';
 
-import ChatHeader from '../../components/block/chatHeader';
-import ChannelDB from '../../components/module/channelDB';
-import ChannelSocket from '../../components/module/channelSocket';
-import MessageDelete from '../../components/ReactModal/messageDelete';
-import {showContext} from '../../context/showContext';
-import UserProfile from '../../components/ReactModal/userProfile';
+import ChatHeader from '../../components/block/chatHeader'; //ここに関しては、header単体の修正をする。
+import DatabaseMessage from '../../components/module/databaseMessage'; //大方問題なし
+import SocketMessage from '../../components/module/SocketMessage'; //大方問題無し
+import MessageDelete from '../../components/ReactModal/messageDelete'; //一旦放置
+import {showContext} from '../../context/showContext';//一旦放置
+import UserProfile from '../../components/ReactModal/userProfile';//一旦放置
+
+import {HandleError,Code401,Code500} from '../../components/module/errorHandler';
 
 const ENDPOINT = 'http://localhost:3001';
-const socket = socketIOClient(ENDPOINT);
+const socketIO = socketIOClient(ENDPOINT);
 
-class Channel extends React.Component {
+/* class Channel extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            isLoggedIn : false,
-            userId : '',
-            username : '',
+            isLoggedIn : false, //個々の使用用途がheaderに対しての送出のみ。header内部でイベントを取得することができればここは不要になる
+            userId : '', //これはsessionが完了すれば入らなくなる可能性もあり
+            username : '', //これはsessionとheaderの整備で不要化するかも（popupの出現に必要な可能性もありだが。。。）
             channel : {},
             chatData : [],
-            chat_message : '',
+            chat_message : '', //これは必要だが、もしかしたら子コンポーネントとして扱うかも
             socket : [],
             show : false,
-            deleteData : {},
+            deleteData : {}, //これもここで管理する必要があるかkな？
             block : {},
             showAccount : false,
             profileExist : false,
@@ -35,9 +38,9 @@ class Channel extends React.Component {
         this.handleChange = this.handleChange.bind(this);
     }
 
-    componentDidMount(){
+    componentDidMount(){//これに関しては、useEffectでのセットを行う。ただし、成功時の処理はそのほかの項目が終わってからしかできなそうか
 
-        var path = this.props.location.pathname;
+        var path = this.props.location.pathname; //この層をもっと簡単にできるかもしれない。location
         const id = path.split('/')[3];
 
         const error = new Error();
@@ -55,7 +58,7 @@ class Channel extends React.Component {
             }
             return res.json();
         }).then((obj) => {
-
+            
             this.setState({
                 isLoggedIn : obj.isLoggedIn,
                 userId : obj.userId,
@@ -114,7 +117,7 @@ class Channel extends React.Component {
                 socket.emit('message',message);
 
                 socket.once("accepter",(data) => {
-                    this.setState(prevState => ({
+                    this.setState(prevState => ({ //そっか、前のsocketのデータもstateに入れ続ける必要があるのか
                         socket : [...prevState.socket,{
                             userId : data.userId,
                             username : data.user,
@@ -130,6 +133,9 @@ class Channel extends React.Component {
 
         const target = e.target;
         target['chat_message'].value = '';
+        this.setState({
+            chat_message : ''
+        })
     }
 
     cancel(e){
@@ -176,19 +182,6 @@ class Channel extends React.Component {
         fetch(`/api/profile/${id}`)
         .then((res) => {
             if(!res.ok){
-                /* console.error('res.ok:',res.ok);
-
-                console.log(res);
-
-
-                console.error('res.status',res.status);
-                console.error('res.statusText:',res.statusText);
-
-                error.status = res.status;
-                error.message = res.statusText;
-                throw error; */
-
-
                 return res.json().then(function(err) {
 
                     error.statusText = err.message;
@@ -252,6 +245,8 @@ class Channel extends React.Component {
     }
 
     render(){
+        console.log(this.state.socket);
+
         if(!this.state.userId){
             return null;
         }else{
@@ -323,7 +318,7 @@ class Channel extends React.Component {
                                 <p>作成者　{this.state.channel.createdBy}</p>
                                 <p>{this.state.message}</p>
                             </div>
-                            <ChannelDB 
+                            <DatabaseMessage 
                                 chatData={this.state.chatData} 
                                 userId={this.state.userId}
                                 profileShow={this.profileShow.bind(this)}
@@ -356,6 +351,126 @@ class Channel extends React.Component {
             )
         }
     }
+} */
+
+
+
+//messageDeleteとuserProfileに関しては、コンポーネントへのイベントの移行ができそう。ただ先にDatabaseMessageとchannelSocketの大きなイベントの対処から行うべきか
+//form送信層を作ってしまうのはありかもしれない。ここで（プラスでupdateとdelete）socketとの接続を限定的に行うようにすることも可能なはず。かなりコンパクトになるかもしれない
+
+function Channel(props){
+    const [userId,setUserId] = useState(''); //これはsessionが適用され次第削除したい。
+    const [username,setUsername] = useState(''); //同様
+    const [text,setText] = useState('');
+    const [channel,setChannel] = useState({});
+    const [chatData,setChatData] = useState([]); //chatのメッセージ管理を行
+    const [socket,setSocket] = useState(''); //socketのメッセージ管理を行う
+    const [message,setMessage] = useState('');
+
+    const history = useHistory();
+    const location = useLocation();
+
+    useEffect(() => {//初期ロード時に読み込むもの
+        var path = location.pathname;
+        var id = path.split("/")[3];
+
+        fetch(`/api/chat/${id}`)
+        .then(HandleError)
+        .then((obj) => {
+            setUserId(obj.userId);
+            setUsername(obj.username);
+            setChannel(obj.channel);
+            setChatData(obj.channel.chatData)
+        })
+        .catch((err) => {
+            if(err.status === 401){
+                Code401(err,history);
+            }else if(err.status === 500){
+                Code500(err,history);
+            }
+        });
+    },[]);
+
+    const handleChange = (e) => { //例えば、送信機能を故コンポーネントに委ねてしまうという手段。これを行うと、可読性の向上にはつながると思うが、
+        const target = e.target;
+        const value = target.value;
+
+        setText(value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        //socketの配列を更新する
+        var date = new Date();
+        var dayGetter = ["日","月","火","水","木","金","土"]; 
+        var customId = uuid();
+        var path = location.pathname;
+        const chatId = path.split('/')[3];
+
+        if(text){
+            if(socketIO !== undefined){
+                const message = {
+                    id : chatId,
+                    userId : userId,
+                    username : username,
+                    text : text,
+                    date : `${date.getFullYear()}年${date.getMonth()+1}月${date.getDate()}日(${dayGetter[date.getDay()]})`,
+                    time : `${date.getHours()}:${date.getMinutes()}`,
+                    customId : customId,
+                }
+
+                socketIO.emit('message',message);
+
+                socketIO.once("accepter",(data) => {
+                    var newSocket = {
+                        userId : data.userId,
+                        username : data.user,
+                        date : data.date,
+                        time : data.time,
+                        text : data.text,
+                        customId : data.customId
+                    }
+
+                    setSocket([...socket,newSocket]);
+                });
+            }
+        }
+
+        const target = e.target;
+        target['text'].value = '';
+        setText('');
+        console.log(`socketInSetText : ${socket}`);
+    }
+
+    return(
+        <div>
+            <ChatHeader isLoggedIn={true} />
+
+            <div className='chat_main'>
+                <div className='channel_information'>
+                    <p>チャンネル名 : {channel.channelName}</p>
+                    <p>チャンネル詳細 : {channel.channelDetail}</p>
+                    <p>作成者 : {channel.createdBy}</p>
+                    <p>{message}</p>
+                </div>
+                <DatabaseMessage 
+                    chatData={chatData} 
+                    userId={userId}
+                />
+                <SocketMessage
+                    socket={socket}
+                    userId={userId}
+                />
+                <form className='message_submit' onSubmit={handleSubmit}>
+                    <input type="text" name='text' onChange={handleChange} />
+                    <input type="submit" value='送信' />
+                </form>
+            </div>
+        </div>
+    )
 }
+
+//一旦初期機能（messageの表示と追加）以外を無視して考える。
 
 export default Channel;
