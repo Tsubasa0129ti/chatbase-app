66b716e972c7d0a84e17b5907e0d250390284102
井上翼 <tsubasa.inoue.3b@stu.hosei.ac.jp>
2022-02-05 19:32:06 +0900

    create message update event but it has a little things what i want to rechange

---
+++ frontend/src/components/atoms/messageUpdate.jsx
@@ -0,0 +1,87 @@
+import React,{useState,useEffect,useRef} from 'react';
+import { useLocation } from 'react-router-dom';
+import socketIOClient from 'socket.io-client';
+
+const ENDPOINT = 'http://localhost:3001';
+const socket = socketIOClient(ENDPOINT);
+
+function MessageUpdate(props){
+    const [value,setValue] = useState('');
+    const form = useRef(null);
+
+    const location = useLocation();
+
+    useEffect(() => {
+        var current = form.current;
+        var text = current.parentNode.parentNode.parentNode.children[2].textContent;
+        setValue(text);
+    },[]);
+
+    const handleChange = (e) => {
+        e.preventDefault();
+
+        var target = e.target;
+        var value = target.value;
+
+        setValue(value);  
+    }
+
+    const Cancel = (e) => { //テキストに関しても、displayを消すので、戻す必要がある。情報が更新されるように設定したい。
+        e.preventDefault();
+
+        var current = form.current;
+        current.style.display = 'none';
+
+        var messageBox = current.parentNode.parentNode.parentNode.children[2];
+        messageBox.style.display = 'block'; //一応これでも可能だけど、流石にこの取得の仕方は可読性等に問題あり、、、
+
+        var text = messageBox.textContent;
+        setValue(text);
+    }
+
+    const Update = (e) => {
+        e.preventDefault();    
+
+        var current = form.current;
+        var block = current.parentNode.parentNode.parentNode;
+        var customId = block.children[3].value;
+
+        var path = location.pathname;
+        var chatId = path.split('/')[3];
+
+        var message = {
+            chatId : chatId,
+            userId : props.userId,
+            customId : customId,
+            newMsg : value
+        }
+
+        socket.emit('update',message);
+
+        socket.once('update',(data) => {
+            block.children[2].textContent = data.text;
+        });
+
+        //displayの処理
+        current.style.display = 'none';
+
+        var messageBox = block.children[2];
+        messageBox.style.display = 'block';
+    }
+
+
+    return(
+        <form ref={form} style={{display:"none"}}>
+            <input type="text" onChange={handleChange} value={value} />
+            <input type="submit" value='Cancel' onClick={Cancel} />
+            <input type="submit" value='Update' onClick={Update} />
+        </form>
+    )
+}
+
+export default MessageUpdate;
+/* 
+修正点
+①domの取得方法が少しややこしいのでこれの改善をする。
+②この画面が表示されているときはpopupが消失しないようにする。でないと、動かした時にmessageだけが存在しないものになってしまう。
+ */
--- frontend/src/components/module/chatPopup.jsx
+++ frontend/src/components/module/chatPopup.jsx
@@ -1,115 +1,21 @@
-import React, { useState,useEffect,useRef } from 'react';
-import socketIOClient from 'socket.io-client';
+import { useState,useEffect,useRef} from 'react';
 
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSmile,faReply,faEdit,faEraser} from '@fortawesome/free-solid-svg-icons';
 
-import MessageEdit from '../../components/atoms/messageEdit';
+import MessageUpdate from '../atoms/messageUpdate';
 
-import {showContext} from '../../context/showContext';
-
-const ENDPOINT = 'http://localhost:3001';
-const socket = socketIOClient(ENDPOINT);
+import {showContext} from '../../context/showContext'; //deleteをどのように行なっていたのかについてふりかえりつつ...
 
 /* class ChatPopup extends React.Component{
-    constructor(props){
-        super(props);
-        this.forEdit = this.forEdit.bind(this);
-    }
-
-    forEdit(e){ //これはまだ未完　最終版は、元のテキストの部分にinputを埋め込む形に変更する
-        e.preventDefault();
-
-        const target = e.currentTarget;
-        var editor = target.closest('.chat_popup').children[4];
-        editor.style.display = 'inline-block';
-    }
-
-    edit(e){
-        const target = e.currentTarget;
-        var newMsg = target.closest('.message_edit').firstChild.value;
-
-        var path = this.props.location.pathname;
-        var chatId = path.split('/')[3];
-
-        var block;
-        var customId;
-        if(!this.props.socket){
-            block = target.closest('.aaa_test');
-            customId = block.children[3].value;
-        }else{
-            block = target.closest('.bbb_test');
-            customId = block.children[3].value;
-        }
-
-        const message = {
-            chatId :  chatId,
-            userId : this.props.userId,
-            customId : customId,
-            newMsg : newMsg,     
-        };
-        socket.emit('update',message);
-
-        socket.once('update',(data) => {
-            block.children[2].textContent = data.text;
-        });
-
-        var editor = target.closest('.message_editor');
-        editor.style.display = 'none';
-        
-    }//初期段階は上記でOK 但し、その後に、inputを現在のテキストと置き換えるということをする あと、二つのクラス名については後で修正を加える予定（ちなみに繰り返し構文のため、今後クラス名の重複は発生するがこれについては後）
-
-    cancel(e){
-        const target = e.currentTarget;
-
-        var editor = target.closest('.message_editor');
-        editor.style.display = 'none';
-    }
-
-    render(){ //この下のコンポーネントは、クリック時の処理　こちらはdisplayのnoneとshowの状態下で繰り返し読み込まれるもの（ただ、おそらくそこまでの負荷はないはず）
-        console.log(this.props.popup);
-        if(!this.props.show){
-            return null;
-        }else{
-            if(this.props.self){
-                return(
-                    <div className='chat_popup'>
-                        <a href="/" className='reaction_button' onClick={this.reaction}>
-                            <img src="reaction_icon" alt="reaction_icon" />リアクションボタン
-                        </a>
-                        <a href='/' className='reply_button' onClick={this.reply}>
-                            <img src='reply_icon' alt='reply_icon' />リプライボタン
-                        </a>
-                        <div>
-                            <a href='/' className='edit_button' onClick={this.forEdit}>
-                                <img src='edit_button' alt='edit_icon' />編集ボタン
-                            </a>
-                        </div>
-                        <div>
-                            <a href='/' className='delete_button' onClick={this.context.showEvent}>
-                                <img src='delete_icon' alt='delete_icon' />削除ボタン
-                            </a>
-                        </div>
-                        <div style={{display:'none'}} className='message_editor'>
-                            <MessageEdit onCancelCallback={this.cancel.bind(this)} onEditCallback={this.edit.bind(this)} />
-                        </div>
-                    </div>
-                )
-            }else{
-                console.log('aaa')
-                return(
-                    <div>
-                        <a href="/" className='reaction_button' onClick={this.reaction}>
-                            <img src="reaction_icon" alt="reaction_icon" />AAA
-                        </a>
-                        <a href='/' className='reply_button' onClick={this.reply}>
-                            <img src='reply_icon' alt='reply_icon' />BBB
-                        </a>
-                    </div>
-                )
-            }
-        }
-        
+    render(){
+        return(
+            <div>
+                <a href='/' className='delete_button' onClick={this.context.showEvent}>
+                    <img src='delete_icon' alt='delete_icon' />削除ボタン
+                </a>
+            </div>  
+        )
     }
 }
 
@@ -119,6 +25,7 @@ ChatPopup.contextType = showContext; */
 function ChatPopup(props){
     const [content,setContent] = useState('');
     const el = useRef(null);
+    const box = useRef(null);
 
     const reaction = (e) => {
         e.preventDefault();
@@ -130,9 +37,18 @@ function ChatPopup(props){
         console.log('replay');
     }
 
-    const update = (e) => {
+    const update = (e) => {//一応現状では、inputの発生とtextの消失をdisplayの変化によって実現している。
         e.preventDefault();
-        console.log('update');
+
+        var target = box.current;//現在獲得可能な最上位を取得
+
+        var parent = target.parentNode;
+        var textElement = parent.children[2];
+        textElement.style.display = 'none';
+
+        var current = box.current;
+        var form = current.children[0].children[4];
+        form.style.display = 'inline-block'
     }
 
     const erase = (e) => {
@@ -142,40 +58,43 @@ function ChatPopup(props){
 
     useEffect(() => {
         var current = el.current;
-        console.log(current);
-        var parent = current.parentNode; //ここがたまにエラーになってしまうparentNodeがエラーになるのならば、currentが受け取れrていない可能性もあるかも
-        var userId = parent.children[0].dataset.user;
 
-        if(props.userId === userId){
-            var popup = (
-                <div className='popup_box'>
-                    <div className='reaction_button'>
-                        <a href="/" onClick={reaction}><FontAwesomeIcon icon={faSmile} /></a>
+        if(current){
+            var parent = current.parentNode;
+            var userId = parent.children[0].dataset.user;
+
+            if(props.userId === userId){
+                var popup = (
+                    <div className='popup_box'>
+                        <div className='reaction_button'>
+                            <a href="/" onClick={reaction}><FontAwesomeIcon icon={faSmile} /></a>
+                        </div>
+                        <div className='reply_button'>
+                            <a href="/" onClick={reply}><FontAwesomeIcon icon={faReply} /></a>
+                        </div>
+                        <div className='edit_button'>
+                            <a href="/" onClick={update}><FontAwesomeIcon icon={faEdit} /></a>
+                        </div>
+                        <div className='delete_button'>
+                            <a href="/" onClick={erase}><FontAwesomeIcon icon={faEraser} /></a>
+                        </div>
+                        <MessageUpdate userId={props.userId} />
                     </div>
-                    <div className='reply_button'>
-                        <a href="/" onClick={reply}><FontAwesomeIcon icon={faReply} /></a>
+                )
+            }else{
+                var popup = (
+                    <div className='popup_box'>
+                        <div className='reaction_button'>
+                            <a href="/" onClick={reaction}><FontAwesomeIcon icon={faSmile} /></a>
+                        </div>
+                        <div className='reply_button'>
+                            <a href="/" onClick={reply}><FontAwesomeIcon icon={faReply} /></a>
+                        </div>
                     </div>
-                    <div className='edit_button'>
-                        <a href="/" onClick={update}><FontAwesomeIcon icon={faEdit} /></a>
-                    </div>
-                    <div className='delete_button'>
-                        <a href="/" onClick={erase}><FontAwesomeIcon icon={faEraser} /></a>
-                    </div>
-                </div>
-            )
-        }else{
-            var popup = (
-                <div className='popup_box'>
-                    <div className='reaction_button'>
-                        <a href="/" onClick={reaction}><FontAwesomeIcon icon={faSmile} /></a>
-                    </div>
-                    <div className='reply_button'>
-                        <a href="/" onClick={reply}><FontAwesomeIcon icon={faReply} /></a>
-                    </div>
-                </div>
-            )
+                )
+            }
+            setContent(popup);
         }
-        setContent(popup);
     },[]);
 
     if(content === ''){
@@ -184,11 +103,10 @@ function ChatPopup(props){
         );
     }else{
         return (
-            <div style={{display:'none'}}>{content}</div>
+            <div ref={box} style={{display:'none'}}>{content}</div>
         )
     }
 }
 
 export default ChatPopup;
-
-//初期ロードの時にcurrentが読み取れない時がある。useRefの取得できるタイミングを知る必要性があるか
+//参考　useRef createRefの二つのパターンがあり、前者はレンダリング時の結果を返すのに対して、後者は常に最新のものを返す
