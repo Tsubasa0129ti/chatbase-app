befd5457c92758d354774aa839a94a3cf651c7f5
井上翼 <tsubasa.inoue.3b@stu.hosei.ac.jp>
2022-02-08 15:46:49 +0900

    add scss in addChannel

--- frontend/src/components/module/addChannel.jsx
+++ frontend/src/components/module/addChannel.jsx
@@ -2,11 +2,14 @@ import {useState} from 'react';
 import {useHistory} from 'react-router-dom';
 import {HandleError,Code401,Code500} from './errorHandler';
 
+import '../../styles/components/ReactModal/addChannel.scss';
+
 function AddChannel(props) {
     const [newChannel,setNewChannel] = useState({
         channelName : '',
         channelDetail : ''
     });
+    const [message,setMessage] = useState('');
     const history = useHistory();
 
     const handleChange = (e) => {
@@ -41,6 +44,9 @@ function AddChannel(props) {
         }).catch((err) => {
             if(err.status === 401){
                 Code401(err,history);
+            }else if(err.status === 422){
+                setMessage(`${err.status} : ${err.type}`);
+                console.log(err);
             }else if(err.status === 500){
                 Code500(err,history);
             }
@@ -51,26 +57,33 @@ function AddChannel(props) {
         return null;
     }else{
         return (
-            <form onSubmit={handleSubmit}>
-                <div className='description'>
-                    <h3>チャンネルを作成する</h3>
-                    <p>チャンネルとはコミュニケーションを取る場所です。特定のトピックに基づいてチャンネルを作ると良いでしょう。</p>
-                </div>
-                <div className='close'>
-                    <a href="/" className='close_button' onClick={props.cancel}>X</a>
-                </div>
-                <div className='channel_name'>
-                    <label htmlFor="channelName">チャンネル名</label>
-                    <input type="text" className='channelName' name='channelName' required onChange={handleChange} />
-                </div>
-                <div className='detail'>
-                    <label htmlFor="channel_detail">
-                        <label htmlFor="channelDetail">チャンネル詳細</label>
-                        <input type="text" className='channelDetail' name='channelDetail' required onChange={handleChange} />
-                    </label>
-                </div>
-                <input type="submit" value='作成' />
-            </form>
+            <div className='overray'>
+                <form className='addChannelModal' onSubmit={handleSubmit}>
+                    <div className='description'>
+                        <h3>チャンネルを作成する</h3>
+                        <p>チャンネルとはコミュニケーションを取る場所です。</p>
+                        <p>特定のトピックに基づいてチャンネルを作ると良いでしょう。</p>
+                    </div>
+                    <div className='close'>
+                        <a href="/" className='close_button' onClick={props.cancel}>X</a>
+                    </div>
+                    <div className='error'>
+                        <p>{message}</p>
+                    </div>
+                    <div className='channel_name'>
+                        <label htmlFor="channelName">チャンネル名</label>
+                        <input type="text" className='channelName' name='channelName' required onChange={handleChange} />
+                    </div>
+                    <div className='detail'>
+                        <label htmlFor="channel_detail">
+                            <label htmlFor="channelDetail">チャンネル詳細</label>
+                            <input type="text" className='channelDetail' name='channelDetail' required onChange={handleChange} />
+                        </label>
+                    </div>
+                    <input type="submit" value='作成' />
+                </form>
+            </div>
+            
         )
     }
 }
