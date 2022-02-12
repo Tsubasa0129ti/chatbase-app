9a029f05860430589f51da96e9316636fe79ea27
井上翼 <tsubasa.inoue.3b@stu.hosei.ac.jp>
2022-02-12 15:42:35 +0900

    revise router and it could use router and provider at the same time.

--- frontend/src/pages/chatPages/index.jsx
+++ frontend/src/pages/chatPages/index.jsx
@@ -6,19 +6,47 @@ import Guide from './guide';
 import Channel from './channel';
 import NotFound from '../errorPages/notFound';
 
-import {DeleteProvider,ProfileProvider} from '../../components/module/store'
+import {DeleteProvider,ProfileProvider,AddChannelProvider} from '../../components/module/store'
 
-function Chat(props) {
+function Chat(props) {//レンダリング層の大規模な修正が必要になった。ちなみに、後でreact-router-domの使い方についてもう少し深ぼってみる。
     return(
         <Switch>
-            <Route path={`${props.url}`} exact component={ChatPage} />
-            <Route path={`${props.url}/search`} exact={true} component={Search} />
+            <Route 
+                path={`${props.url}`} 
+                exact
+                render={() => {
+                    return(
+                        <AddChannelProvider>
+                            <ChatPage />
+                        </AddChannelProvider>
+                    )
+                }}
+            />
+            <Route
+                path={`${props.url}/search`}
+                exact
+                render={() => {
+                    return(
+                        <AddChannelProvider>
+                            <Search />
+                        </AddChannelProvider>
+                    ) 
+                }}
+            />
             <Route path={`${props.url}/guide`} exact component={Guide} />
-            <DeleteProvider>
-                <ProfileProvider>
-                    <Route path={`${props.url}/page/:id`} exact component={Channel} />
-                </ProfileProvider>    
-            </DeleteProvider>
+            <Route 
+                path={`${props.url}/page/:id`}
+                exact
+                render={() => {
+                    return (
+                        <DeleteProvider>
+                            <ProfileProvider>
+                                <Channel />
+                            </ProfileProvider>
+                        </DeleteProvider>
+                    ) 
+                }}
+            />
             <Route component={NotFound} />
         </Switch>  
     )
